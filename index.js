cf663f6f7ba4ac40f6ab96c1a977a406b16743db
Toffielicious <91135758+Toffielicious@users.noreply.github.com>
2023-04-05 19:55:55 +0800

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Masakit ulo ko</h1>`;
 
 fetchPosts().then(posts => {
   posts.forEach(post => {
